4b63c3ec32a3e2fc014c471aa21fb1a10f1d52eb
Forkerm <krizanking95@gmail.com>
2026-03-06 20:37:56 +0545

    johpjkbjihjh

---
+++ TIMESTAMPPPPPP.py
@@ -0,0 +1,10 @@
+from datetime import date, time, datetime
+
+todayy = date.today()
+now = datetime.now()
+
+print("Today's Date: ",todayy)
+
+print("Current date and time: ",now)
+
+print("Dtae componets: ",todayy.year,todayy.month,todayy.day)
